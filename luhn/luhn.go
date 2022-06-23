45d985814eebdc22f8a712e0bcf9c754d77a9de2
Fellipe Souto <fllsouto@gmail.com>
2022-06-23 15:01:09 -0300

    exercise: luhn iteration 2

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -2,7 +2,6 @@
 package luhn
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -20,27 +19,20 @@ func Valid(id string) bool {
 		idArray = append(idArray, v)
 
 		if err != nil {
-			fmt.Printf("Error! %s", err.Error())
 			return false
 		}
 	}
-	fmt.Printf("B idArray: %v\n", idArray)
 	for i := len(idArray) - 2; i >= 0; i = i - 2 {
-		//fmt.Printf("[%d] -- %d\n", i, idArray[i])
 		tmp := idArray[i] * 2
 		if tmp > 9 {
 			tmp -= 9
 		}
 
 		idArray[i] = tmp
-		//fmt.Printf("[%d] -- %d\n", i, idArray[i])
 	}
-	fmt.Printf("A idArray: %v\n", idArray)
 	result := 0
 	for _, x := range idArray {
 		result += x
 	}
-	validation := (result%10 == 0)
-	fmt.Printf("Result: %d -- Validation %t\n\n", result, validation)
-	return validation
+	return (result%10 == 0)
 }
