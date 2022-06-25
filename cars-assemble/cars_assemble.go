be5cec46a5ccc8a9f94d8b8d81049ec002f1ea1b
Fellipe Souto <fllsouto@gmail.com>
2022-06-25 19:40:02 -0300

    exercise: cars assemble iteration 1

---
+++ cars-assemble/cars_assemble.go
@@ -0,0 +1,24 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * (successRate / 100.0)
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
+}
+
+// CalculateCost works out the cost of producing the given number of cars
+func CalculateCost(carsCount int) uint {
+	individualCost := 10000
+	groupCost := 95000
+
+	numberOfGroups := (carsCount / 10)
+	numberOfIndividualCars := (carsCount % 10)
+
+	return uint((numberOfGroups * groupCost) + (numberOfIndividualCars * individualCost))
+}
